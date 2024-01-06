a9367d5b856ebef13a60a51bb7858712625fe68e
croko22 <kvchambit@gmail.com>
2024-01-06 12:49:30 -0500

    Simple refactor in main

--- main.c
+++ main.c
@@ -3,7 +3,6 @@
 #include "src/algorithms/sjf.c"
 #include "src/algorithms/rr.c"
 
-//? SCRIPT: gcc main.c  src/utils/proc.h src/utils/proc.c  && ./a.out
 int main()
 {
     int option;
@@ -12,13 +11,12 @@ int main()
     printf("2. SJF\n");
     printf("3. Priority\n");
     printf("4. Round Robin\n");
-    printf("5. Exit\n");
     scanf("%d", &option);
+
     int num_processes;
-
     struct proc_info *processes = get_proc_info(&num_processes);
-    printf("%-10s %-20s %-10s %-15s %-15s %-20s\n", "PID", "Name", "Priority", "Burst Time", "Arrival Time", "Normalized Burst Time");
 
+    printf("%-10s %-20s %-10s %-15s %-15s %-20s\n", "PID", "Name", "Priority", "Burst Time", "Arrival Time", "Normalized Burst Time");
     for (int i = 0; i < num_processes; i++)
     {
         printf("%-10d %-20s %-10d %-15lld %-15ld %-20d\n", processes[i].pid, processes[i].name, processes[i].priority, processes[i].burst_time, processes[i].arrival_time, processes[i].normalized_burst_time);
@@ -37,15 +35,11 @@ int main()
     case 4:
         rr(processes, num_processes);
         break;
-    case 5:
-        return 0;
     default:
         printf("Invalid option\n");
-        break;
+        return 1;
     }
 
-    // fcfs(processes, num_processes);
-    printf("Number of processes: %d\n", num_processes);
-
+    // printf("Number of processes: %d\n", num_processes);
     return 0;
 }
