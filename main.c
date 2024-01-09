86db1508a886a9b490bfc9bc5c17b9d22abe2aa5
croko22 <kvchambit@gmail.com>
2024-01-09 18:44:41 -0500

    Run all at once

--- main.c
+++ main.c
@@ -5,14 +5,6 @@
 
 int main()
 {
-    int option;
-    printf("Choose an option:\n");
-    printf("1. FCFS\n");
-    printf("2. SJF\n");
-    printf("3. Priority\n");
-    printf("4. Round Robin\n");
-    scanf("%d", &option);
-
     int num_processes;
     struct proc_info *processes = get_proc_info(&num_processes);
 
@@ -22,24 +14,23 @@ int main()
         printf("%-10d %-20s %-10d %-15lld %-15ld %-20d\n", processes[i].pid, processes[i].name, processes[i].priority, processes[i].burst_time, processes[i].arrival_time, processes[i].normalized_burst_time);
     }
 
-    switch (option)
-    {
-    case 1:
-        fcfs(processes, num_processes);
-        break;
-    case 2:
-        sjf(processes, num_processes);
-        break;
-    case 3:
-        break;
-    case 4:
-        rr(processes, num_processes);
-        break;
-    default:
-        printf("Invalid option\n");
-        return 1;
-    }
+    // Make copies of the original processes array for each algorithm
+    struct proc_info *processes_fcfs = malloc(sizeof(struct proc_info) * num_processes);
+    struct proc_info *processes_sjf = malloc(sizeof(struct proc_info) * num_processes);
+    struct proc_info *processes_rr = malloc(sizeof(struct proc_info) * num_processes);
+    memcpy(processes_fcfs, processes, sizeof(struct proc_info) * num_processes);
+    memcpy(processes_sjf, processes, sizeof(struct proc_info) * num_processes);
+    memcpy(processes_rr, processes, sizeof(struct proc_info) * num_processes);
 
-    // printf("Number of processes: %d\n", num_processes);
+    // Run each algorithm
+    fcfs(processes_fcfs, num_processes);
+    sjf(processes_sjf, num_processes);
+    rr(processes_rr, num_processes);
+
+    // Free the memory allocated for the copies
+    free(processes_fcfs);
+    free(processes_sjf);
+    free(processes_rr);
+
     return 0;
 }
