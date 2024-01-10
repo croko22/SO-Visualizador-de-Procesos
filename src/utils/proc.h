19d1dba824929a6339dabaea750a3cbd1c024f1b
croko22 <kvchambit@gmail.com>
2024-01-10 14:00:47 -0500

    Comment on process values

--- src/utils/proc.h
+++ src/utils/proc.h
@@ -3,15 +3,15 @@
 
 struct proc_info
 {
-    int pid;
-    char name[256];
-    int priority;
-    long arrival_time;
-    long long burst_time;
-    int normalized_burst_time;
-    int execution_time;
-    int waiting_time;
-    int turnaround_time;
+    int pid;                   // Identificador del proceso
+    char name[256];            // Nombre del proceso
+    int priority;              // Prioridad del proceso
+    long arrival_time;         // Tiempo de llegada del proceso
+    long long burst_time;      // Tiempo de ráfaga del proceso
+    int normalized_burst_time; // Tiempo de ráfaga normalizado del proceso
+    int execution_time;        // Tiempo de ejecución del proceso
+    int waiting_time;          // Tiempo de espera del proceso
+    int turnaround_time;       // Tiempo de retorno del proceso
 };
 
 struct proc_info *get_proc_info(int *count);
