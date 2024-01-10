814dd4988995f5337986233f4aff17b1a4332437
croko22 <kvchambit@gmail.com>
2024-01-10 07:17:30 -0500

    Delete print

--- main.c
+++ main.c
@@ -8,12 +8,6 @@ int main()
     int num_processes;
     struct proc_info *processes = get_proc_info(&num_processes);
 
-    printf("%-10s %-20s %-10s %-15s %-15s %-20s\n", "PID", "Name", "Priority", "Burst Time", "Arrival Time", "Normalized Burst Time");
-    for (int i = 0; i < num_processes; i++)
-    {
-        printf("%-10d %-20s %-10d %-15lld %-15ld %-20d\n", processes[i].pid, processes[i].name, processes[i].priority, processes[i].burst_time, processes[i].arrival_time, processes[i].normalized_burst_time);
-    }
-
     // Make copies of the original processes array for each algorithm
     struct proc_info *processes_fcfs = malloc(sizeof(struct proc_info) * num_processes);
     struct proc_info *processes_sjf = malloc(sizeof(struct proc_info) * num_processes);
